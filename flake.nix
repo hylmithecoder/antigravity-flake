446affdeeb8524f4560dd32d2df537fbefef4f03
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2026-05-22 09:37:42 +0700

    Antigravity flake 2.0.2

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Google Antigravity - Next-generation agentic IDE (Nix package)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        packages = {
+          default = pkgs.callPackage ./package.nix {};
+          google-antigravity = pkgs.callPackage ./package.nix {};
+          google-antigravity-no-fhs = pkgs.callPackage ./package.nix {useFHS = false;};
+        };
+
+        # Development shell
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nix
+            git
+            curl
+            jq
+          ];
+
+          shellHook = ''
+            echo "Google Antigravity 2.0.2 Flake Environment"
+          '';
+        };
+      }
+    )
+    // {
+      # Version information
+      version = "2.0.2-5949548972081152";
+
+      # Overlay for easy integration into configurations
+      overlays.default = final: prev: {
+        google-antigravity = final.callPackage ./package.nix {};
+        google-antigravity-no-fhs = final.callPackage ./package.nix {useFHS = false;};
+      };
+    };
+}
